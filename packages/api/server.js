ca05966297d16bc992760218dc1df87c1ce7741e
Madelyndubuk <dubuk@usc.edu>
2020-06-16 16:54:27 -0700

    fix: removed extra line from server.js

--- packages/api/server.js
+++ packages/api/server.js
@@ -69,5 +69,4 @@ const port = process.env.PORT ? Number(process.env.PORT) : 3000;
 const host = '0.0.0.0';
 const server = app.listen(port, host, () => console.log(`Example app listening at http://localhost:${port}`));
 
-
 module.exports = server;
