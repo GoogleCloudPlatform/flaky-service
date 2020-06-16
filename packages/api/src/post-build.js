69496dbf011c38ee0915599a3cca574939014d1e
Stuart Rucker <stuart.a.rucker@gmail.com>
2020-06-16 11:56:03 -0700

    Added robust error handling

--- packages/api/src/post-build.js
+++ packages/api/src/post-build.js
@@ -13,6 +13,9 @@
 // See the License for the specific language governing permissions and
 
 // class to receive POSTS with build information
+
+// NOTE: relies on global.headCollection to be the high level repository
+
 const addBuild = require('../src/add-build');
 const TestCaseRun = require('../lib/testrun');
 
@@ -24,7 +27,7 @@ class PostBuildHandler {
 
   parseBuildInfo (metadata) {
     // buildInfo must have attributes of organization, timestamp, url, environment, buildId
-    return {
+    var returnVal = {
       repoId: encodeURIComponent(metadata.github.repository),
       organization: metadata.github.repository_owner,
       timestamp: new Date(metadata.github.event.head_commit.timestamp),
@@ -32,20 +35,36 @@ class PostBuildHandler {
       environment: metadata.os.os,
       buildId: metadata.github.sha
     };
+
+    // validate data
+    for (var prop in returnVal) {
+      if (!returnVal[prop]) {
+        throw new Error('Missing All Build Meta Data Info - ' + prop);
+      }
+    }
+
+    return returnVal;
   }
 
   parseTestCases (data) {
     return data.map(function (x) {
+      if (typeof x.ok !== 'boolean' || !x.id || !x.name) {
+        throw new Error('Missing All Test Case Info');
+      }
       return new TestCaseRun(x.ok ? 'ok' : 'not ok', x.id, x.name);
     });
   }
 
   listen () {
-    this.app.post('/postbuild', (req, res) => {
-      var buildInfo = this.parseBuildInfo(req.body.metadata);
-      var testCases = this.parseTestCases(req.body.data);
-      addBuild(testCases, buildInfo, this.client, 'post-build-testing');
-      res.send({ message: 'goodnight moon' });
+    this.app.post('/postbuild', async (req, res, next) => {
+      try {
+        var buildInfo = this.parseBuildInfo(req.body.metadata);
+        var testCases = this.parseTestCases(req.body.data);
+        await addBuild(testCases, buildInfo, this.client, global.headCollection);
+        res.send({ message: 'successfully added build' });
+      } catch (err) {
+        res.status(400).send({ error: err.message });
+      }
     });
   }
 }
