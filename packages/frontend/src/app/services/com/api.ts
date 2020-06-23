ff24b77b98677626b30462a71a38395417a02000
Madelyndubuk <dubuk@usc.edu>
2020-06-22 21:46:55 -0700

    testing windows.location.origin api base

--- packages/frontend/src/app/services/com/api.ts
+++ packages/frontend/src/app/services/com/api.ts
@@ -18,7 +18,7 @@ interface ApiLinks {
   };
 }
 
-const apiBaseLink = 'http://localhost:3000/';
+const apiBaseLink = 'window.location.origin';
 export const apiLinks: ApiLinks = {
   get: {
     repositories: apiBaseLink + 'repos',
