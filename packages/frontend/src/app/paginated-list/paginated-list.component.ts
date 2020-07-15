e83f16d40556bb6ea64f26f0d6f6fe83a9b6c2e5
Ismael Gbian <66028195+gis727@users.noreply.github.com>
2020-07-14 23:27:01 -0400

    feat: added pagination only when needed (#76)

--- packages/frontend/src/app/paginated-list/paginated-list.component.ts
+++ packages/frontend/src/app/paginated-list/paginated-list.component.ts
@@ -27,6 +27,7 @@ export class PaginatedListComponent<Elements> implements OnInit {
   renderedElements: Elements[] = [];
   pageIndex = 0;
   pageSize = 10;
+  showPaginator = false;
 
   constructor(
     public sanitizer: DomSanitizer,
@@ -52,6 +53,7 @@ export class PaginatedListComponent<Elements> implements OnInit {
       this.pageSize = page.pageSize;
     }
     this.renderedElements = this._elements.slice(startIndex, endIndex);
+    this.showPaginator = this._elements.length > this.pageSize;
   }
 
   onElementClick(link: string, event: MouseEvent) {
