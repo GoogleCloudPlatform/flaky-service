6b371b2772e208e3c90babb8128293fc10330b68
Madelyndubuk <dubuk@usc.edu>
2020-07-21 09:44:21 -0700

    added COMService import back

--- packages/frontend/src/app/repository/repository.component.spec.ts
+++ packages/frontend/src/app/repository/repository.component.spec.ts
@@ -30,7 +30,7 @@ import {NoopAnimationsModule} from '@angular/platform-browser/animations';
 import {By} from '@angular/platform-browser';
 import {expectedParams} from '../services/interpretation/interpretation.service';
 import {RouteProvider} from '../routing/route-provider/RouteProvider';
-import {COMService} from'../services/
+import {COMService} from'../services/com/com.service';
 
 // Mock the inner components
 @Component({
