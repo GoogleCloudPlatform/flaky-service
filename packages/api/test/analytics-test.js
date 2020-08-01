7df6ce9f645e0e8f57d7031c105513d562b0f7cb
Stuart Rucker <stuartrucker@google.com>
2020-07-31 17:47:18 -0700

    feat!: optimize uploading of data (#182)

--- packages/api/test/analytics-test.js
+++ packages/api/test/analytics-test.js
@@ -99,32 +99,7 @@ describe('Flaky-Analytics', () => {
       }
     });
 
-    it('Can re-add builds', async () => {
-      const updateObj = JSON.parse(JSON.stringify(buildInfo));
-      updateObj.timestamp = new Date('01/01/2020');
-      updateObj.buildId = '0';
-      const testCaseObjs = [];
-      for (let k = 0; k < testCases[0].length; k++) {
-        testCaseObjs.push(new TestCaseRun(testCases[0][k] ? 'ok' : 'not ok', k, k.toString()));
-      }
-
-      // readding the first build should mantain its status as nonflaky
-      // since only looking at test cases with timestamps prior
-      await addBuild(testCaseObjs, updateObj, client, global.headCollection);
-
-      // buildata not changed
-      const buildData = await client.collection(global.headCollection).doc(buildInfo.repoId).collection('builds').where('buildId', '==', '0').get();
-      let result;
-      buildData.forEach(r => { result = r; });
-      assert.strictEqual(result.data().flaky, 0);
-
-      // test case flakyness not changed
-      const testFlakyExpecations = [false, true, true, false];
-      for (let i = 0; i < testFlakyExpecations.length; i++) {
-        const testData = await client.collection(global.headCollection).doc(buildInfo.repoId).collection('tests').doc(i.toString()).get();
-        assert.strictEqual(testData.data().flaky, testFlakyExpecations[i]);
-      }
-    });
+    // cannot readd builds
 
     it('Can add builds non chronologically', async () => {
       const updateObj = JSON.parse(JSON.stringify(buildInfo));
@@ -142,7 +117,7 @@ describe('Flaky-Analytics', () => {
       const buildData = await client.collection(global.headCollection).doc(buildInfo.repoId).collection('builds').where('buildId', '==', '1.5').get();
       let result;
       buildData.forEach(r => { result = r; });
-      assert.strictEqual(result.data().flaky, 1); // only 1 is flaky at this point
+      assert.strictEqual(result.data().flaky, 2); // only 1 is flaky looking backwards, however two fit the criteria of currently being flaky and failing on that build.
 
       // ensure test cases flakyness is updated
       const testFlakyExpecations = [false, true, true, true]; // note test case 4 has now become flaky
