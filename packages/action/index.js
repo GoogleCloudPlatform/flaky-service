bb03e793fcec8d6c860601f7d617ca49802ccefd
Stuart Rucker <stuart.a.rucker@gmail.com>
2020-08-06 10:43:34 -0700

    warn test

--- packages/action/index.js
+++ packages/action/index.js
@@ -47,8 +47,14 @@ async function main() {
       buildmessage: buildmessageStr
     };
 
-    console.log(metaData);
+    metaData.matrix = JSON.stringify(metadata.matrix, Object.keys(metadata.matrix).sort()); //consistancy 
+    
+    metaData.environment.ref = envData.ref.replace('refs/', '');
+    metaData.environment.ref = envData.ref.replace('heads/', '');
 
+    console.log(metaData);
+    core.warning("WARN TEST");
+    
     const fileType = core.getInput('logtype');
     
     if (!fs.existsSync(core.getInput('filepath'))){
