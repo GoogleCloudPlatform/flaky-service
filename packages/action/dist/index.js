991872cc554122376caaefbb22b3a41358fe6880
Stuart Rucker <stuart.a.rucker@gmail.com>
2020-08-06 17:45:42 -0700

    stop early

--- packages/action/dist/index.js
+++ packages/action/dist/index.js
@@ -191,6 +191,7 @@ async function main() {
     // `who-to-greet` input defined in action metadata file
     if(!core.getInput('repo-token') || !core.getInput('os') || !core.getInput('file-path:') || !core.getInput('repo-token') ){
       core.setFailed("Github action missing required fields. Refer to documentation at https://github.com/googlecloudplatform/flaky-service");
+      return;
     }
 
    
