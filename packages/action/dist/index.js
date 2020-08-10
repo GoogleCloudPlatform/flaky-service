d38061384a0c59d1a8f3aa6b2f3f4a7e7f97b903
Stuart Rucker <stuart.a.rucker@gmail.com>
2020-08-10 15:22:50 -0700

    ruiuniong action endpoint

--- packages/action/dist/index.js
+++ packages/action/dist/index.js
@@ -232,7 +232,7 @@ async function main () {
     const data = fs.readFileSync(
       core.getInput('file-path'), 'utf8');
     const sendMe = { type: fileType, data: data, metadata: metaData };
-    const endpoint = core.getInput('endpoint') + '/api/build/gh/v1';
+    const endpoint = core.getInput('endpoint') + '/api/build/gh/v1RANDOM';
     console.log('Beginning Upload of data...');
     console.log(JSON.stringify(sendMe));
     console.log(endpoint);
