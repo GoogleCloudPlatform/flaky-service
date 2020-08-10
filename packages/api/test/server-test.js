80abbad3d3bc4c4ab54937a9244adcad3e1c690c
Cedonia Peterson <cedoniap@google.com>
2020-08-10 16:34:12 -0400

    fix server test

--- packages/api/test/server-test.js
+++ packages/api/test/server-test.js
@@ -42,7 +42,7 @@ describe('flaky express server', () => {
   describe('delete /repo', async () => {
     it('generates a GitHub redirect', async () => {
       stubs.push(sinon.stub(repo, 'storeTicket').returns(true));
-      const resp = await fetch('http://0.0.0.0:3000/api/repo/deleteurl/my-org/my-repo/test/my-test?redirect=' + process.env.FRONTEND_URL, {
+      const resp = await fetch('http://0.0.0.0:3000/api/repo/deleteurl/my-org/my-repo/test?testname=my-test&redirect=' + process.env.FRONTEND_URL, {
         method: 'GET'
       });
       const respJSON = await resp.text();
@@ -53,7 +53,7 @@ describe('flaky express server', () => {
       const stubbed = sinon.stub(repo, 'storeTicket');
       stubs.push(stubbed);
 
-      await fetch('http://0.0.0.0:3000/api/repo/deleteurl/my-org/my-repo/test/my-test?redirect=' + process.env.FRONTEND_URL, {
+      await fetch('http://0.0.0.0:3000/api/repo/deleteurl/my-org/my-repo/test&testname=my-test&redirect=' + process.env.FRONTEND_URL, {
         method: 'GET'
       });
 
