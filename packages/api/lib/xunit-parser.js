4c857e7c4b166f9d2cba569c2f95e3aefb595627
Cedonia Peterson <cedoniap@google.com>
2020-08-17 20:06:48 -0400

    initial xunit-parser work

---
+++ packages/api/lib/xunit-parser.js
@@ -0,0 +1,91 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+const xmljs = require('xml-js');
+const {readFileSync} = require('fs');
+
+
+const parse = (xmlString) => {
+	const obj = xmljs.xml2js(xmlString, {compact: true});
+  const failures = [];
+  const passes = [];
+  // Python doesn't always have a top-level testsuites element.
+  let testsuites = obj['testsuite'];
+  if (testsuites === undefined) {
+    testsuites = obj['testsuites']['testsuite'];
+  }
+  if (testsuites === undefined) {
+    return {passes: [], failures: []};
+  }
+  // If there is only one test suite, put it into an array to make it iterable.
+  if (!Array.isArray(testsuites)) {
+    testsuites = [testsuites];
+  }
+  for (const suite of testsuites) {
+    // Ruby doesn't always have _attributes.
+    const testsuiteName = suite['_attributes'] ? suite['_attributes'].name : undefined;
+    let testcases = suite['testcase'];
+    // If there were no tests in the package, continue.
+    if (testcases === undefined) {
+      continue;
+    }
+    // If there is only one test case, put it into an array to make it iterable.
+    if (!Array.isArray(testcases)) {
+      testcases = [testcases];
+    }
+    for (const testcase of testcases) {
+    	console.log("test case!");
+      let pkg = testsuiteName;
+      if (
+        !testsuiteName ||
+        testsuiteName === 'pytest' ||
+        testsuiteName === 'Mocha Tests'
+      ) {
+        pkg = testcase['_attributes'].classname;
+      }
+      // Ignore skipped tests. They didn't pass and they didn't fail.
+      if (testcase['skipped'] !== undefined) {
+        continue;
+      }
+      const failure = testcase['failure'];
+      const error = testcase['error'];
+      if (failure === undefined && error === undefined) {
+        passes.push({
+          package: pkg,
+          testCase: testcase['_attributes'].name,
+          passed: true,
+        });
+        continue;
+      }
+      // Here we must have a failure or an error.
+      let log = failure === undefined ? error['_text'] : failure['_text'];
+      // Java puts its test logs in a CDATA element.
+      if (log === undefined) {
+        log = failure['_cdata'];
+      }
+      failures.push({
+        package: pkg,
+        testCase: testcase['_attributes'].name,
+        passed: false,
+        log,
+      });
+    }
+  }
+  return {
+    passes: passes,
+    failures: failures,
+  };
+}
+
+console.log(parse(readFileSync(require.resolve('../test/fixtures/one_failed.xml'), 'utf8')));
---
+++ packages/api/test/xunit-parser-test.js
@@ -0,0 +1,25 @@
+// Copyright 2020 Google LLC
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//     https://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+const { describe, it, afterEach } = require('mocha');
+const assert = require('assert');
+const parser = require('../lib/xunit-parser');
+const {readFileSync} = require('fs');
+
+describe('xunit-parser-test', () => {
+	it('parses xunit into Javascript object', () => {
+		const tests = parser(readFileSync(require.resolve('./fixtures/one_failed.xml'), 'utf8'));
+		assert.strictEqual(tests.length, 4);
+	});
+});
